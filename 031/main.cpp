9cf43d9253d6bbcb8a3318b7cc6b828b94ebbdba
mike.lee <mike.lee.china@gmail.com>
2023-10-28 21:01:13 +0800

    bugfix: 031 output formate

--- 031/main.cpp
+++ 031/main.cpp
@@ -2,14 +2,5 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	double average = 9.6;
-	int judges = 6;
-	double noHi = 9.4;
-	double noLow = 9.8;
-
-	double all = average * judges;
-	double hi = all - noHi * (judges - 1);
-	double low = all - noLow * (judges - 1);
-
-	cout << (all - hi - low)/(judges - 2) << endl;
+	printf("%6.2f",(9.4*5 + 9.8*5 - 9.6*6)/4);
 }
