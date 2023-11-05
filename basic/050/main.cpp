dc03552386925fedfa73b5652dc3d7ffe986fe4c
mike.lee.china <mke.lee.china@gmail.com>
2023-11-05 10:00:21 +0800

    feature:050

---
+++ basic/050/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	char c;
+	cin>>c;
+	cout<<"  "<<c<<endl;
+	cout<<" "<<c<<c<<c<<endl;
+	cout<<c<<c<<c<<c<<c<<endl;
+	return 0;
+}
