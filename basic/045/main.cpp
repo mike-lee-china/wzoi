d71b22a7424050157dfc34ed98aac261d7c50554
mike.lee.china <mke.lee.china@gmail.com>
2023-11-05 09:30:55 +0800

    feature: 045

---
+++ basic/045/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	long long a;
+	int b;
+	short c;
+	cout << sizeof(a) << " " << sizeof(b) << " " << sizeof(c) << endl;
+	return 0;
+}
