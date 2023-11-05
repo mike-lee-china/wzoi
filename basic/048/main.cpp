7374318dd5ef8c0142393e620329fc5833197aef
mike.lee.china <mke.lee.china@gmail.com>
2023-11-05 09:52:44 +0800

    feature:048

---
+++ basic/048/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	char a;
+	int b;
+	float c;
+	double d;
+	cin>>a>>b>>c>>d;
+
+	printf("%c %d %.6f %.6f\n", a, b, c, d);
+	return 0;
+}
