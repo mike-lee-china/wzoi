59d3d702a40331828923f74b6e20e66fa8bf7812
mike.lee <mike.lee.china@gmail.com>
2023-12-10 21:11:15 +0800

    refo: using do while

--- basic/081/main.cpp
+++ basic/081/main.cpp
@@ -2,18 +2,22 @@
 #include <iostream>
 using namespace std;
 
-int main(int, char**){
+int main(int, char**) {
     double len, time;
     cin>>len>>time;
     double price = 11.0;
-    price += (int)time/3;
-    if (len <= 2) {
-        price += 0;
-    } else if (len <= 10) {
-        price += (len - 2) * 1.8;
-    } else {
+    price += time/3;
+    do {
+        if (len <= 2) {
+            break;
+        } 
+        if (len <= 10) {
+            price += (len - 2) * 1.8;
+            break;
+        } 
+        
         price += 8 * 1.8 + (len - 10) * 1.8 * 1.5;
-    }
-    printf("%.1f\n",price);
-    return 0;    
+    }while(0);
+    printf("%.1f\n", price);
+    return 0;
 }
